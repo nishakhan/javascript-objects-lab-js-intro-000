d9424fc296dbbdb9225d25b8ccd44b468fb563aa
Nisha Khan <nisha.f.khan@gmail.com>
2020-07-29 22:58:15 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 function deleteFromObjectByKey(object, key) {
 
   var newObject = Object.assign({}, object)
-  delete newObject.key
+  delete newObject[key]
   return newObject
 }
